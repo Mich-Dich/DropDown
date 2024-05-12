dbcd251bc9a5bbafa1595f56166626d1bc106ddc
Marius <marius.bretzel@icloud.com>
2024-05-12 11:01:48 +0200

    Added player texture

--- Projektarbeit/player/player.cs
+++ Projektarbeit/player/player.cs
@@ -11,7 +11,7 @@ namespace Projektarbeit.player {
         public CH_player() {
             
             transform.size = new Vector2(100);
-            set_sprite(new sprite(resource_manager.get_texture("assets/textures/player/00.png")));
+            set_sprite(new sprite(resource_manager.get_texture("assets/textures/player/Angel-1.png")));
             add_collider(new collider(collision_shape.Circle)
                 .set_offset(new transform(Vector2.Zero, new Vector2(-10)))
                 .set_physics_material(new physics_material(10.05f, 0.1f)));
