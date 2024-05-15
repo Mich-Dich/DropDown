4b5680a9116abfdae1b756d7f8a03684e8c4289e
Marius <marius.bretzel@icloud.com>
2024-05-15 09:22:18 +0200

    Worked on enemy

--- DropDown/enemy/CH_small_bug.cs
+++ DropDown/enemy/CH_small_bug.cs
@@ -11,19 +11,16 @@ namespace DropDown.enemy {
 
         public CH_small_bug() {
 
-            ray_number = 15;
-            ray_cast_range = 800;
+            ray_number = 1;
+            ray_cast_range = 1000;
             ray_cast_angle = float.Pi/2;
-            auto_detection_range = 400;
-            attack_range = 150;
-            damage = 5;
+            auto_detection_range = 500;
+            attack_range = 300;
+            damage = 10;
 
-            attack_anim_notify_frame_index = 21;
             attack_anim = new animation_data("assets/animation/small_bug/attack_01.png", 8, 3, true, false, 30, true);
             walk_anim = new animation_data("assets/animation/small_bug/walk.png", 8, 4, true, false, 80, true);
             idle_anim = new animation_data("assets/animation/small_bug/idle_01.png", 16, 10, true, false, 30, true);
         }
-
-
     }
 }
